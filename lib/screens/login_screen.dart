1c882b014f344cb956b1ff3c598f90bb39620457
Murat <murattaksimm@gmail.com>
2023-08-20 21:21:36 +0300

    Login UI Design

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/constants.dart';
+
+class LoginScreen extends StatelessWidget {
+  final userNameController = TextEditingController();
+
+  LoginScreen({Key? key}) : super(key: key);
+
+  //Widget for user input
+  Widget inputWidgetUser(TextEditingController userInput, String hintTitle,
+      TextInputType keyboardType) {
+    return Container(
+      height: 55,
+      margin: const EdgeInsets.only(bottom: 15),
+      decoration: BoxDecoration(
+          color: Colors.blueGrey.shade100,
+          borderRadius: BorderRadius.circular(30)),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 25.0, top: 15, right: 25),
+        child: TextField(
+          controller: userInput,
+          autocorrect: false,
+          enableSuggestions: false,
+          autofocus: false,
+          decoration: InputDecoration.collapsed(
+            hintText: hintTitle,
+            hintStyle: const TextStyle(
+                fontSize: 18,
+                color: Colors.white70,
+                fontStyle: FontStyle.italic),
+          ),
+          keyboardType: keyboardType,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment.topCenter,
+            fit: BoxFit.fill,
+            image: AssetImage('assets/background.png'),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: 510,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 40),
+                    inputWidgetUser(userNameController, 'UserName',
+                        TextInputType.text),
+                    Container(
+                      height: 55,
+                      padding:
+                      const EdgeInsets.only(top: 5, left: 70, right: 70),
+                      child: OutlinedButton(style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25)),
+                        backgroundColor: primaryColor,
+                      ),
+                        onPressed: () {
+                          if (kDebugMode) {
+                            print(userNameController.text);
+                          }
+                        },
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: Text('Please enter your User Name...',
+                        style: TextStyle(color: primaryColor),),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
