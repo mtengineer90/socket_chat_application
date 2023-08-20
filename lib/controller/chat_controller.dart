6e3e9faa74735ed836838be2f8c9e7f339878eb2
Murat <murattaksimm@gmail.com>
2023-08-20 21:36:27 +0300

    GetX State Management Implementation

---
+++ lib/controller/chat_controller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class ChatController extends GetxController {
+
+  //Socketsbay socket service connection
+  final channel = WebSocketChannel.connect(
+    //Uri.parse('wss://socketsbay.com/wss/v2/10/349cd0f881cc1c9bd5e4f819ca4733d4/'),
+    Uri.parse('wss://socketsbay.com/wss/v2/1/demo/'),
+  );
+
+  //Channel Sink Close on Dispose
+  @override
+  void dispose() {
+    super.dispose();
+    channel.sink.close();
+  }
+
+  //Initialization
+  @override
+  void onInit() {
+    super.onInit();
+  }
+}
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+import '../controller/chat_controller.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -9,35 +11,33 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
-  //Socketsbay socket service connection
-  final channel = WebSocketChannel.connect(Uri.parse(
-      'wss://socketsbay.com/wss/v2/1/demo/'
-  ));
+  //Getx ChatController
+  final ChatController chatController = Get.put(ChatController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Socket Chat')),
-      body: StreamBuilder(
-        stream: channel.stream,
-        builder: (ctx, dataSnapshot) {
-          if (dataSnapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (dataSnapshot.hasError) {
-            return Text(
-              'ERROR: ${dataSnapshot.error}',
-              style: const TextStyle(color: Colors.red),
-            );
-          }
-          return Text(dataSnapshot.hasData ? '${dataSnapshot.data}' : '');
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          channel.sink.add('Message Sent!');
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Chat Data from Socket :',
+                ),
+                StreamBuilder(
+                  stream: chatController.channel.stream,
+                  builder: (context, snapshot) {
+                    return Text(snapshot.hasData ? '${snapshot.data}' : '', style: Theme.of(context).textTheme.bodyLarge,);
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
